df062cdb4f34336a7670322f77f19b4fadc10d64
Malyshko Serhii <malyshkosergiy@gmail.com>
2020-09-17 19:37:41 +0300

    add baguettebox.js

---
+++ src/js/slider.js
@@ -0,0 +1,7 @@
+import 'baguettebox.js/dist/baguetteBox.min.css';
+
+import baguetteBox from 'baguettebox.js';
+baguetteBox.run('.baguetteBoxThree', {
+  animation: 'fadeIn',
+  noScrollbars: true,
+});
